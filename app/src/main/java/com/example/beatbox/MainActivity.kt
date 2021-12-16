d1ed9ebec39630fa62ab9be403ffd71242ebd35b
Prilepskiy <woolf7754286@yandex.ru>
2021-12-16 16:25:42 +0700

    mod recyclerView and play soulds

--- app/src/main/java/com/example/beatbox/MainActivity.kt
+++ app/src/main/java/com/example/beatbox/MainActivity.kt
@@ -11,30 +11,47 @@ import com.example.beatbox.databinding.ActivityMainBinding
 import com.example.beatbox.databinding.ListItemSoundBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var beatBox:BeatBox
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        beatBox= BeatBox(assets)
+      //  beatBox.loadsSounds()
         val binding: ActivityMainBinding=DataBindingUtil.setContentView(this,R.layout.activity_main)
         binding.recyclerView.apply {
             layoutManager=GridLayoutManager(context,3)
-            adapter=SoundAdapter()
+            adapter=SoundAdapter(beatBox.sounds)
         }
     }
     private inner class SoundHolder(private val binding: ListItemSoundBinding): RecyclerView.ViewHolder(binding.root)
     {
-
+        init {
+            binding.viewModel= SoundViewModel(beatBox)
+        }
+        fun bind(sound: Sound){
+            binding.apply {
+                viewModel?.sound=sound
+                executePendingBindings()
+            }
+        }
     }
 
-    private inner class SoundAdapter():RecyclerView.Adapter<SoundHolder>(){
+    override fun onDestroy() {
+        super.onDestroy()
+        beatBox.release()
+    }
+
+    private inner class SoundAdapter(private val sounds: List<Sound>):RecyclerView.Adapter<SoundHolder>(){
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SoundHolder {
             val binding=DataBindingUtil.inflate<ListItemSoundBinding>(layoutInflater,R.layout.list_item_sound,parent,false)
             return SoundHolder(binding)
         }
 
         override fun onBindViewHolder(holder: SoundHolder, position: Int) {
-            TODO("Not yet implemented")
+            val sound=sounds[position]
+            holder.bind(sound)
         }
 
-        override fun getItemCount(): Int =0
+        override fun getItemCount(): Int =sounds.size
 
     }
 
